86d3c6c09f37563e53f5d4fb8f86f19e5e0ab3a8
Alessio1921 <caruso.alessio1999@gmail.com>
2022-04-07 15:20:48 +0200

    initialized the project and completed the header

---
+++ src/components/Header.vue
@@ -0,0 +1,109 @@
+<template>
+  <header>
+    <div class="container">
+      <div class="row">
+        <div class="col-12 d-flex justify-content-between">
+          <div class="header-logo ms-5">
+            <img src="../assets/fable/images/logo.png" alt="">
+          </div>
+          <div class="right-header">
+            <ul class="list-unstyled d-flex me-5">
+              <li class="element-header p-4 text-center" :class="(element.active) ? 'active' : ''" v-for="(element,index) in headerList" :key="index">
+                <img :src="require(`../assets/fable/`+element.imgActive)" alt="" v-if="element.active">
+                <img :src="require(`../assets/fable/`+element.img)" alt="" v-else>
+                <p>{{element.text}}</p>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'indexHeader',
+  props: {
+  },
+  data:function(){
+    return{
+      headerList:[
+        {
+          text:"Home",
+          active:true,
+          img:"home.png",
+          imgActive:"home-2.png",
+        },
+        {
+          text:"Pages",
+          active:false,
+          img:"document.png",
+          imgActive:"document-2.png",
+        },
+        {
+          text:"Blog",
+          active:false,
+          img:"printer.png",
+          imgActive:"printer-2.png",
+        },
+        {
+          text:"Shop",
+          active:false,
+          img:"cart.png",
+          imgActive:"cart-2.png",
+        },
+        {
+          text:"Shortcodes",
+          active:false,
+          img:"lab.png",
+          imgActive:"lab-2.png",
+        },
+        {
+          text:"Support",
+          active:false,
+          img:"chat.png",
+          imgActive:"chat-2.png",
+        },
+        {
+          text:"Contact",
+          active:false,
+          img:"envelope.png",
+          imgActive:"envelope-2.png",
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.container,
+.row,
+.col-12,
+.header-logo{
+  height: 100px;
+}
+header{
+  height: 100%;
+  .container{
+    .row{
+      .col-12{
+        overflow: hidden;
+        .header-logo{
+          img{
+            height: 100%;
+          }
+        }
+        ul{
+          li.active{
+            background-color: #fe6601;
+            color: white;
+          }
+        }
+      }
+    }
+  }
+}
+</style>
