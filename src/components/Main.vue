f838944289dbe595c57989ecf41cb532df39dd2a
Alessio1921 <caruso.alessio1999@gmail.com>
2022-04-08 11:44:22 +0200

    added section overvwiew and component carousel

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -49,20 +49,57 @@
       </div>
     </section>
     <!-- section overview -->
-    <section id="overview">
+    <section id="overview" class="mt-5">
       <div class="my-container d-flex">
+        <!-- left part -->
         <div class="left">
-
+          <div class="left-top d-flex">
+            <div class="title orange text-white">
+              <h6>Overview</h6> 
+              <div class="arrow-down"></div>
+            </div>
+            <div class="title border-bottom border-2 ms-3">
+              <h6>Our Mission</h6> 
+            </div>
+          </div>
+          <h3 class="my-5">Our philosophy is learning through play as we offer a stimulating environment for children.</h3>
+          <div class="left-bottom">
+            <!-- left elements -->
+            <div class="element d-flex mb-3">
+              <div class="circle orange mx-auto me-3">
+                <img src="../assets/fable/images/clock_alt.png" alt="clock img">
+              </div>
+              <div class="description">
+                <h3>Full Day Session</h3>
+                <p>Pulvinar est metro liglua blandit maecenas retrum gravida cuprum.Maecenas node estibulum</p>
+              </div>
+            </div>
+            <div class="element d-flex">
+              <div class="circle orange mx-auto me-3">
+                <img src="../assets/fable/images/diagram_alt.png" alt="diagram img">
+              </div>
+              <div class="description">
+                <h3>Varied Classes</h3>
+                <p>Pulvinar est metro liglua blandit maecenas retrum gravida cuprum.Maecenas node estibulum</p>
+              </div>
+            </div>
+          </div>
         </div>
-        <div class="right"></div>
+        <div class="right">
+          <Carousel />
+        </div>
       </div>
     </section>
   </main>
 </template>
 
 <script>
+import Carousel from './Carousel.vue'
 export default {
   name: 'indexMain',
+  components: {
+    Carousel,
+  },
   props: {
   }
 }
@@ -100,7 +137,7 @@ export default {
               }
           }
           .element-text{
-            width: 250px;
+            width: 215px;
             h5{
               color: $violet;
             }
@@ -110,8 +147,55 @@ export default {
     }
     // overview
     #overview{
+      background-color: $white;
       .left{
         width: 50%;
+        h3{
+          color: $violet;
+        }
+        .title.orange{
+          background-color: $orange;
+        }
+        .title{
+          padding:.5rem 1.5rem;
+          .arrow-down{
+            width: 0px;
+            height: 0px;
+            border-left: 10px solid transparent;
+            border-right: 10px solid transparent;
+            border-top: 10px solid $orange;
+            position:relative;
+            top: 1rem;
+            left: 50%;  
+            transform: translateX(-50%);
+          }
+          h6{
+            margin-bottom: 0;
+            padding-top: 0.5rem;
+          }
+        }
+        .left-bottom{
+          .circle.orange{
+            background-color: $orange;
+          }
+          .circle{
+            padding: 20px;
+            width: 90px;
+            height: 70px;
+            border-radius: 50%;
+            position: relative;
+            img{
+              position: absolute;
+              height: 40%;
+              top: 50%;
+              left: 50%;
+              transform: translate(-50%,-50%);
+            }
+          }
+          h3{
+            color:$violet;
+          }
+        }
       };
       .right{
         width: 50%;
